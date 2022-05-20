6e07aa952474f6307ba95960b3fb5c93a6df59dd
sthecabral <sthecabral17@gmail.com>
2022-05-19 23:26:17 -0300

    feature(main): half of code description added

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
+//Atribuindo um elemento do DOM (imagem do personagem) à variável character através de um seletor, pela classe
 const character = document.getElementsByClassName("character")[0];
+//Atribuindo um elemento do DOM (div da imagem) à variável character através de um seletor, pela classe
 const containerCharacter = document.getElementsByClassName("container-character")[0];
 
-
+//Declarando a velocidade com que o personagem irá se mover a cada tecla
 const VELOCITY = 10;
 
+//Variável que representa o espaço horizontal disponível para movimentação do personagem
 const WINDOW_WIDTH = window.innerWidth - 100;
+//Variável que representa o espaço vertical disponível para movimentação do personagem
 const WINDOW_HEIGHT = window.innerHeight - 100;
+//Em ambas acima substitui do objeto screen por window e subtrai do tramanho character para aumentar a precisão da medida
 
+//Variáveis mutáveis que representam a posição do character na página, analogamente ao plano cartesiano:
+//Esse seria o X do par ordenado
 let xPosition = 500;
+//Esse seria o Y do par ordenado
 let yPosition = 300;
 
+//Array contendo os possíveis eventos a serem recebidos através das setas do teclado
 const keysAvaiable = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
+//Array contendo as classes que podem ser ativadas no documento CSS a depender do evento recebido
 const directions = ["turnUp", "turnLeft", "turnRight", "turnDown"];
 
 window.addEventListener("keydown", (event) => {
