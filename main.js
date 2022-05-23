768a7f01fef3e8326ab152832c5591c6b6d4c23c
sthecabral <sthecabral17@gmail.com>
2022-05-22 22:40:31 -0300

    feature(main): final description added

--- main.js
+++ main.js
@@ -23,45 +23,68 @@ const keysAvaiable = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
 //Array contendo as classes que podem ser ativadas no documento CSS a depender do evento recebido
 const directions = ["turnUp", "turnLeft", "turnRight", "turnDown"];
 
+//Adiciona um eventListener no DOM Object "Window" que recebe como parametros o evento "keydown" 
+//para identificar quando uma tecla for pressionada no teclado e também recebe uma arrow function
+//que está sendo declarada, a qual, por sua vez, recebe o parametro "event" que representa o keyboardEvent 
 window.addEventListener("keydown", (event) => {
+    //Variável que guarda o atributo "key" que retorna o valor da tecla pressionada no keyboardEvent
     const key  = event.key;
 
+    //Verifica se a tecla pressionada no keyboardevent corresponde a alguma daquelas declaradas no array keysAvaible. 
+    //obtem-se essa verificação através do método some que varre o array executando uma arrow function que
     const keyPressedAvaiable =  keysAvaiable.some((currentKey) => {
+        //compara cada valor do array ao valor da variável key e guarda a conclusão em boolean na variável keyPressedAvaiable
         return currentKey === key;
     })
 
+    //Estrutura condicional, caso a tecla pressionada pelo usuário não seja nenhuma das esperadas a função retornará vazia,
+    //o que acontecerá caso keyPressedAvailble retorne false
     if(!keyPressedAvaiable) return;
 
+    //Usa o método forEach para executar a arrow function em cada elemento do array "directions"
     directions.forEach((direction) => {
+        //Estrutura condiiconal, caso o HTML Element de class "character" tenha também outra classe listada que corresponda 
+        //a uma daquelas declaradas em "directions", essa será removida de sua classlist
         if(character.classList.contains(direction)) character.classList.remove(direction);
     })
 
-
+    //Estrutura condicional, caso a tecla pressionada seja a seta para cima
     if(key === "ArrowUp"){
+        //Será adicionada a class ".turnUp" ao HTML Element "character" no index.html
         character.classList.add("turnUp");
+        //E será diminuído 10 pixeis da posição vertical do elemento na página
         yPosition -= VELOCITY;
     }
-
+    //Estrutura condicional, caso a tecla pressionada seja a seta para baixo
     if(key === "ArrowDown"){
+        //Será adicionada a class ".turnDown" ao HTML Element "character" no index.html
         character.classList.add("turnDown");
+        //E será adicionado 10 pixeis na posição vertical do elemento na página
         yPosition += VELOCITY;
     }
-
+    //Estrutura condicional, caso a tecla pressionada seja a seta para esquerda
     if(key === "ArrowLeft"){
+        //Será adicionada a class ".turnLeft" ao HTML Element "character no index.html
         character.classList.add("turnLeft");
+        //E será diminuído 10 pixeis da posição horizontal do elemento na página
         xPosition -= VELOCITY;
     }
-
+    //Estrutura condicional, caso a tecla pressionada seja a seta para direita
     if(key === "ArrowRight"){
+        //Será adicionada a class ".turnRight" ao HTML Element "character no index.html
         character.classList.add("turnRight");
+         //E será adicionado 10 pixeis da posição horizontal do elemento na página
         xPosition += VELOCITY;
     }
 
+    //Estrutura condicional, caso o valor obtido para posição vertical do elemento esteja entre 0 e a altura máxima da janela
     if(yPosition > 0 && yPosition < WINDOW_HEIGHT){
+        //então a posição do limite superior do elemento é aleterada pela variável yPosition
         containerCharacter.style.top = `${yPosition}px`;
     }
-    
+    //Estrutura condicional, caso o valor obtido para posição horizontal do elemento esteja entre 0 e a largura máxima da janela
     if(xPosition > 0 && xPosition < WINDOW_WIDTH){
+        //então a posição do limite esquerdo do elemento é aleterada pela variável xPosition
         containerCharacter.style.left = `${xPosition}px`
     }
     
